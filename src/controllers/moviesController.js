5fa349d64d836f712aed32217ef601b856465785
axellbustos <axell.bustos@hotmail.com.ar>
2022-09-28 19:45:35 -0300

    routes added

--- src/controllers/moviesController.js
+++ src/controllers/moviesController.js
@@ -1,6 +1,7 @@
 const db=require('../database/models')
 
 module.exports = {
+    //LIST ALL MOVIES
     list : (req,res) => {
         db.Movie.findAll()
             .then((movies)=>{
@@ -8,6 +9,7 @@ module.exports = {
             })
             .catch(error => console.log(error))
     },
+    //NEWEST MOVIES
     new : (req,res) => {
         db.Movie.findAll({
             order:[
@@ -18,6 +20,8 @@ module.exports = {
                 return res.render("newestMovies",{movies})
             })
     },
+
+    //RECOMMENDED MOVIES
     recommended : (req,res) => {
         db.Movie.findAll({
             order:[
@@ -30,15 +34,35 @@ module.exports = {
                 return res.render("recommendedMovies", {movies})
             })
     },
+
+    //DETAIL OF MOVIES
     detail : (req,res) => {
         db.Movie.findByPk(req.params.id)
         .then(movie=> res.render('moviesDetail',{movie}))
         .catch(error => console.log(error))
     },
+
+    //LOAD NEW MOVIE
     add: (req,res)=>{
 
     },
     create: (req,res)=>{
+        
+    },
+
+    //EDIT MOVIE
+    edit: (req,res)=>{
+        
+    },
+    update: (req,res)=>{
+        
+    },
+
+    //DELETE MOVIE
+    delete: (req,res)=>{
+        
+    },
+    destroy: (req,res)=>{
         
     }
 }
--- src/routes/moviesRoutes.js
+++ src/routes/moviesRoutes.js
@@ -2,12 +2,23 @@ const express = require('express');
 const router = express.Router();
 const moviesController = require('../controllers/moviesController');
 
+//VIEWS
 router.get('/movies', moviesController.list);
 router.get('/movies/new', moviesController.new);
 router.get('/movies/recommended', moviesController.recommended);
 router.get('/movies/detail/:id', moviesController.detail);
+
+//LOAD
 router.get('/movies/add', moviesController.add);
 router.post('/movies/create', moviesController.create);
+
+//EDIT
+router.get('/movies/edit/:id', moviesController.edit);
+router.post('/movies/update/:id', moviesController.update);
+
+//DELETE
+router.get('/movies/delete/:id', moviesController.delete);
+router.post('/movies/delete/:id', moviesController.destroy);
 
 
 module.exports = router;
